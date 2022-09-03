34f72c9edf56b224d694a8eec2179fbde611139f
danielxiong <ewall1106@gmail.com>
2022-09-03 23:26:54 +0800

    feat: custom 404 page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,7 @@
-import { ConfigProvider } from 'antd'
 import 'styles/reset.css'
 import 'styles/antd.css'
 
 import type { AppProps } from 'next/app'
-
-ConfigProvider.config({
-  theme: {
-    primaryColor: '#000'
-  }
-})
-
-setTimeout(() => {
-  ConfigProvider.config({
-    theme: {
-      primaryColor: 'green'
-    }
-  })
-}, 3000)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
