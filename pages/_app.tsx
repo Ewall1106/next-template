1b98f38b1b38e05f7956717c89bb029c94425fbe
danielxiong <ewall1106@gmail.com>
2022-09-03 23:00:37 +0800

    feat: import reset.css && ant

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import { ConfigProvider } from 'antd'
+import 'styles/reset.css'
+import 'styles/antd.css'
+
+import type { AppProps } from 'next/app'
+
+ConfigProvider.config({
+  theme: {
+    primaryColor: '#000'
+  }
+})
+
+setTimeout(() => {
+  ConfigProvider.config({
+    theme: {
+      primaryColor: 'green'
+    }
+  })
+}, 3000)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
